93c50a7054fc428ef1a7845e4588256260fcb4b0
Andrei Olar <andrei.olar@gmail.com>
2016-02-21 12:55:45 +0200

    Add tests for SysUtils

--- data-access/src/main/java/ro/samlex/reelcash/Reelcash.java
+++ data-access/src/main/java/ro/samlex/reelcash/Reelcash.java
@@ -1,5 +1,5 @@
 package ro.samlex.reelcash;
-public class Reelcash {
+public interface Reelcash {
     public static final String APPLICATION_NAME = "reelcash";
     public static final String APPLICATION_FRIENDLY_NAME = "Reel₵ash invoicing software";
     public static final String COMPANY_DATA_FILE_NAME = "company_data.json";
